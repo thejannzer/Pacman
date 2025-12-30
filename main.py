848029256d3bcace3568ff9da28d9cc2f7393caa
thejannzer <scholz.carlsfeld@gmail.com>
2025-12-30 22:17:00 +0100

    implemented maze

--- main.py
+++ main.py
@@ -1,15 +1,29 @@
 #main
-import maze 
-import pacman
-import ghost
-import pellet
+from maze import Maze
+#from pacman import Pacman
+#from ghost import Ghost
+#from pellet import Pellet
+import pygame
 
-class main:
-    def __init__(self):
-        self.maze = maze()
-        self.pacman = pacman()
-        self.ghost = ghost()
-        self.running = True
+pygame.init()
 
-        
+maze = Maze()
+#pacman = Pacman()
+#ghost = Ghost()
+running = True
+
+screen = pygame.display.set_mode((maze.WIDTH, maze.HEIGHT))
+pygame.display.set_caption("Pac-Man")
+
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill((0, 0, 0))
+    maze.draw_map(screen, maze.LEVEL)
+    pygame.display.flip()
+pygame.quit()
+
 
