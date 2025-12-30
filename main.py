011b6114762bd006e4fcac02cebedc030b2641aa
thejannzer <scholz.carlsfeld@gmail.com>
2025-12-30 19:01:05 +0100

    added def__init__

---
+++ main.py
@@ -0,0 +1,15 @@
+#main
+import maze 
+import pacman
+import ghost
+import pellet
+
+class main:
+    def __init__(self):
+        self.maze = maze()
+        self.pacman = pacman()
+        self.ghost = ghost()
+        self.running = True
+
+        
+
